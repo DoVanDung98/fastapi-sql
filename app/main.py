c1fb443e92ace6cd9200003da51f5d4ed2e9b2a3
Đỗ Văn Dũng <dungdv34@MacBook-Pro-cua-Nguyen.local>
2025-02-06 17:05:44 +0700

    add jwt token to project for authenticate

--- app/main.py
+++ app/main.py
@@ -8,14 +8,13 @@ import time
 from sqlalchemy.orm import Session
 from sqlalchemy.sql.functions import mode
 from . import models, schemas, utils
-from .database import engine, SessionLocal
+from .database import engine, SessionLocal, get_db
+from .routers import post, user, auth
 
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
 
 
 while True:
@@ -44,6 +43,10 @@ def find_index_post(id):
         if p['id']==id:
             return i
         
+app.include_router(post.router)
+app.include_router(user.router)
+app.include_router(auth.router)
+
 @app.get("/")
 def root():
     return {"message": "Hello world"}
--- app/routers/post.py
+++ app/routers/post.py
@@ -4,33 +4,36 @@ from ..database import engine, SessionLocal, get_db
 from .. import models, schemas, utils
 from sqlalchemy.orm import Session
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/posts",
+    tags=["Posts"]
+)
 
-@router.get("/posts", response_model=List[schemas.Post])
+
+@router.get("/", response_model=List[schemas.Post])
 def get_posts(db: Session=Depends(get_db)):
     posts = db.query(models.Post).all()
-    return {"data": posts}
+    return posts
 
-
-@router.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 def create_posts(post: schemas.PostCreate, db: Session=Depends(get_db)):
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-    return {"data": new_post}
+    return new_post
 
 
-@router.get("/posts/{id}", response_model=schemas.Post)
+@router.get("/{id}", response_model=schemas.Post)
 def get_post(id: int, db: Session=Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id==id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} was not found")
-    return {"post_detail": post}
+    return post
 
 
-@router.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session=Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id==id)
     if post.first()==None:
@@ -41,7 +44,7 @@ def delete_post(id: int, db: Session=Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@router.put("/posts/{id}", response_model=schemas.Post)
+@router.put("/{id}", response_model=schemas.Post)
 def update_post(id: int, updated_post: schemas.PostCreate, db: Session=Depends(get_db)):
     post_query = db.query(models.Post).filter(models.Post.id==id)
     post = post_query.first()
@@ -52,4 +55,4 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session=Depends(g
     
     post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
-    return {"data": post_query.first()}
+    return post_query.first()
